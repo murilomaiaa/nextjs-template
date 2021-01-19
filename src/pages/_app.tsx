bef19e5fa1f7dc8f14ba6a81bb09bbc457d3411f
Murilo Maia <murilomaia.bb@gmail.com>
2021-01-19 16:59:33 -0300

    refactor: removing styled components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,8 @@
 import { AppProps } from 'next/app'
-import { ThemeProvider } from 'styled-components'
-import GlobalStyle from '../styles/GlobalStyle'
-import theme from '../styles/theme'
 
-const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+const MyApp = ({ Component, pageProps }) => {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <Component {...pageProps} />
   )
 }
 
